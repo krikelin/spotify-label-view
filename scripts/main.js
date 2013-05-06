35dd227a1266785033300a934f3c8755f5b10a9f
Alexander Forselius <alexander.forselius@artistconnector.com>
2013-05-06 12:54:53 +0200

    Encapsulated Group into new Group model
    Change-Id: I73e28a5189aecc7643c916be527b2e32157a28d8

--- scripts/main.js
+++ scripts/main.js
@@ -8,108 +8,81 @@ require([
  	'$views/buttons',
  	'$api/search#Search',
  	'sp://label/scripts/views#PlayView',
- 	'$api/library#Library'
-], function(mainStrings, models, Image, TabBar, List, buttons, Search, PlayView, Library) {
+ 	'$api/library#Library',
+ 	'sp://label/scripts/models'
+], function(mainStrings, models, Image, TabBar, List, buttons, Search, PlayView, Library, omodels) {
   'use strict';
-  function getIdentifier(label) {
-  	if(label == 'record union') {
-  		return "recordunionmusic";
-  	}
-  	if(label == 'krikelin') {
-  		return "drsounds"
-  	}
-  	return label;
-  }
-  function getSearch(label) {
-  		if(label == 'krikelin') {
-  			return 'label:krikelin OR artist:\"Dr. Sounds\"';
-  		}
-  		return 'label:' + label;
-  }
-  // We use the data-string attribute to localize elements
-  var localizedElements = document.querySelectorAll('[data-string]');
-  	for(var i = 0; i < localizedElements.length; i++) {
-  		var elm = localizedElements[i];
-  		console.log(elm);
-  		elm.innerHTML = mainStrings.get(elm.dataset['string']);
-  	}
-  	var loadLabel = function (args) {
-  		var label = {
-  			title: args[0],
-  			identifier: args[0]
-  		};
-  		// Clean
-  		document.getElementById('lTopList').innerHTML = "";
-  		document.getElementById('logo').innerHTML = "";
-  		document.getElementById('playlists').innerHTML = "";
+  
+      // We use the data-string attribute to localize elements
+      var localizedElements = document.querySelectorAll('[data-string]');
+      	for(var i = 0; i < localizedElements.length; i++) {
+      		var elm = localizedElements[i];
+      		console.log(elm);
+      		elm.innerHTML = mainStrings.get(elm.dataset['string']);
+      	}
+      	var loadLabel = function (args) {
+          var label = new omodels.Label('spotify:label:krikelin');
+          label.load(['name', 'playlists', 'image']).done(function (label) {
+        	 alert("A");
+        		// Clean
+        		document.getElementById('lTopList').innerHTML = "";
+        		document.getElementById('logo').innerHTML = "";
+        		document.getElementById('playlists').innerHTML = "";
 
-  		document.getElementById('toolbar').innerHTML = "";
-  		document.getElementById('artists').innerHTML = "";
+        		document.getElementById('toolbar').innerHTML = "";
+        		document.getElementById('artists').innerHTML = "";
 
-  		var user = models.User.fromURI('spotify:user:' + getIdentifier(label.identifier));
-  		if(user) {
-  			var avatar = Image.forUser(user, {width: 128, height: 128, style: 'rounded', 'placeholder': 'user', 'title': label.title});
-  			document.getElementById('logo').appendChild(avatar.node);
-  			user.load('name').done(function (user) {
-  				console.log(user);
-  				document.getElementById("ltitle").innerHTML = mainStrings.get('title', user.name.decodeForText());
-  			});
-  			var subscribeButton = buttons.SubscribeButton.forUser(user);
-  			document.getElementById('toolbar').appendChild(subscribeButton.node);
+        		
+      			var avatar = Image.fromSource(label.image, {width: 128, height: 128, style: 'rounded', 'placeholder': 'user', 'title': label.title});
+      			document.getElementById('logo').appendChild(avatar.node);
+      			if(label.user) {
+      				document.getElementById("ltitle").innerHTML = mainStrings.get('title', user.name.decodeForText());
+      			
+      			   var subscribeButton = buttons.SubscribeButton.forUser(user);
+      			   document.getElementById('toolbar').appendChild(subscribeButton.node);
+            }
+            if(label.playlists) {
+        			label.playlists.snapshot(0, 50).done(function (playlists) {
+                console.log('d', playlists.length);
+                for(var i = 0; i < playlists.length; i++) {
+                  console.log('a');
+                  var playlist = playlists.get(i);
+                  if(playlist == null)
+                    continue;
+                  console.log("P", playlist);
+                  var playView = PlayView.forPlaylist(playlist);
+                  console.log('f', playView);
+                  document.getElementById('playlists').appendChild(playView.node);
+                  console.log(playView.node);
+                  playView.init();
+                }
+              });
+  	  		  }
+        	
+        		// Logo for label
+        		
+        			// Create list of top tracks
+        	
+        		var lTopList = List.forCollection(label.tracks, {throbber: 'hide-content', 'layout': 'toplist', 'style': 'rounded', 'numItems': 10});
+        		document.getElementById('lTopList').appendChild(lTopList.node);
+        		lTopList.init();
+        		lTopList.focus();
 
-  			// Get playlists for view
-  			var library = Library.forUser(user);
-  			library.load('published').done(function (library) {
-  				console.log('t', library);
-  				library.published.snapshot(0, 50).done(function (playlists) {
-  					console.log('d', playlists.length);
-  					for(var i = 0; i < playlists.length; i++) {
-  						console.log('a');
-  						var playlist = playlists.get(i);
-  						if(playlist == null)
-  							continue;
-  						console.log("P", playlist);
-	  					var playView = PlayView.forPlaylist(playlist);
-	  					console.log('f', playView);
-	  					document.getElementById('playlists').appendChild(playView.node);
-	  					console.log(playView.node);
-	  					playView.init();
-	  				}
-  				});
-  			});
-	  		
-  		} else {
-  			var avatar = Image.fromSource('img/label.png', {width: 128, height: 128, style: 'rounded', 'placeholder': 'user', 'title': label.title});
-  			document.getElementById('logo').appendChild(avatar.node);
-  			document.getElementById("ltitle").innerHTML = mainStrings.get('title', label.title.decodeForText());
-  		
-  		}
-  		// Logo for label
-  		
-  		
-  		var search = Search.search(getSearch(label.identifier));
-  		
-  		// Create list of top tracks
-  	
-  		var lTopList = List.forCollection(search.tracks, {throbber: 'hide-content', 'layout': 'toplist', 'style': 'rounded', 'numItems': 5});
-  		document.getElementById('lTopList').appendChild(lTopList.node);
-  		lTopList.init();
-  		lTopList.focus();
-
-  		var lTopArtists = List.forCollection(search.artists, {throbber: 'hide-content', 'type': 'artists', 'style': 'rounded', 'header': 'no', 'numItems': 10, 'fields':['image', 'artist']});
-  		document.getElementById('artists').appendChild(lTopArtists.node);
-  		lTopArtists.init();
-  	};
-  var tabBar = TabBar.withTabs([
-    {id: 'overview', name: 'Overview', active: true}
-  ]);
-  tabBar.addToDom(document.getElementById('header'), 'after');
-  // Find items
-  models.application.addEventListener('arguments', function () {
-  	models.application.load('arguments').done(function (a) {
-  		loadLabel(a.arguments);
-  	});
-  })
-  loadLabel(['krikelin']);
-
+        		var lTopArtists = List.forCollection(label.artists, {throbber: 'hide-content', 'type': 'artists', 'style': 'rounded', 'header': 'no', 'numItems': 10, 'fields':['image', 'artist']});
+        		document.getElementById('artists').appendChild(lTopArtists.node);
+        		lTopArtists.init();
+          });
+      	};
+      var tabBar = TabBar.withTabs([
+        {id: 'overview', name: 'Overview', active: true}
+      ]);
+      tabBar.addToDom(document.getElementById('header'), 'after');
+      // Find items
+      models.application.addEventListener('arguments', function () {
+      	models.application.load('arguments').done(function (a) {
+      		loadLabel(a.arguments);
+      	});
+      })
+      loadLabel(['krikelin']);
+    
 });
--- scripts/models.js
+++ scripts/models.js
@@ -1,4 +1,72 @@
+/**
+models
+@module
+**/
 require([
-	'$views/list#List', 
-	'$views/image#Image', 
-	'$views/buttons#SubscribeButton'], 
+	'$api/models',
+	'$api/search#Search',
+	'$api/library#Library'],
+	function (models, Search, Library) {
+		/**
+		Label
+		@class
+		@inherits Loadable
+		**/
+		exports.Label = function (uri) {
+			function getIdentifier(label) {
+			  	if(label == 'record union') {
+			  		return "recordunionmusic";
+			  	}
+			  	if(label == 'krikelin') {
+			  		return "drsounds"
+			  	}
+			  	return label;
+			}
+		 	function getSearch(label) {
+			  		if(label == 'krikelin') {
+			  			return 'label:krikelin OR artist:\"Dr. Sounds\"';
+			  		}
+			  		if(label == 'spotify') {
+			  			return 'year:0-3000';
+			  		}
+			  		return 'label:' + label;
+		  	}
+			console.log("uri", uri);
+			this.uri = uri;
+			var self = this;
+			if(!uri.indexOf('spotify:label:') == 0) {
+				throw "Invalid URI";
+			}
+			this._load = function (e) {
+				var labelId = this.uri.split(':')[2];
+				this.resolve('name', labelId);
+				this.resolve('image', 'img/label.png');
+				var search = Search.search(getSearch(labelId));
+				this.resolve('tracks', search.tracks);
+				this.resolve('albums', search.albums);
+				this.resolve('artists', search.artists);
+				var user = models.User.fromURI('spotify:user:' + getIdentifier(labelId));
+				if(user) {
+					user.load('name', 'image').done(function (user) {
+						self.resolve('user', user);
+						self.resolve('name', user.name);
+						// Get playlists for view
+			  			var library = Library.forUser(user);
+			  			library.load('published').done(function (library) {
+			  				console.log('t', library);
+			  				self.resolve('playlists', library.published);
+			  				self.resolveDone(); 
+			  			});
+			  			self.resolveDone(); 
+					});
+				} else {
+					self.resolve('image', 'img/label.png');
+					self.resolveDone(); 
+				}
+			};
+		};
+		exports.Label.prototype = new models.Loadable();
+		models.Loadable.define(exports.Label, ['name', 'tracks', 'albums', 'playlists', 'image', 'artists'], '_load');
+
+
+});
