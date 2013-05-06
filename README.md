b6b05e31827269c902a53f55256c99c95a8eb7c4
Alexander Forselius <alexander.forselius@artistconnector.com>
2013-05-06 09:32:40 +0200

    Added top list and label view
    Change-Id: Id01055c6b239888ee253e39c9f764890ed1c53a8

---
+++ README.md
@@ -0,0 +1,2 @@
+# Label view for Spotify
+This is the missing label view for Spotify that will make a nicer way to browse labels on Spotify.
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,60 @@
+
 require([
-  '$api/models',
-  '$views/image#Image'
-], function(models, Image) {
+	'strings/main.lang',
+  	'$api/models',
+ 	'$views/image#Image',
+ 	'$views/tabbar#TabBar',
+ 	'$views/list#List',
+ 	'$api/search#Search',
+], function(mainStrings, models, Image, TabBar, List, Search) {
   'use strict';
 
-  // the code of your app
+  // We use the data-string attribute to localize elements
+  var localizedElements = document.querySelectorAll('[data-string]');
+  	for(var i = 0; i < localizedElements.length; i++) {
+  		var elm = localizedElements[i];
+  		console.log(elm);
+  		elm.innerHTML = mainStrings.get(elm.dataset['string']);
+  	}
+  	var loadLabel = function (args) {
+  		var label = {
+  			title: args[0],
+  			identifier: args[0]
+  		};
+  		// Clean
+  		document.getElementById('lTopList').innerHTML = "";
+  		document.getElementById('logo').innerHTML = "";
+
+
+  		var user = models.User.fromURI('spotify:user:' + label.identifier);
+  		if(user) {
+  			var avatar = Image.forUser(user, {width: 128, height: 128, style: 'rounded', 'placeholder': 'user', 'title': label.title});
+  			document.getElementById('logo').appendChild(avatar.node);
+  		} else {
+  			var avatar = Image.fromSource('img/label.png', {width: 128, height: 128, style: 'rounded', 'placeholder': 'user', 'title': label.title});
+  			document.getElementById('logo').appendChild(avatar.node);
+  		}
+  		// Logo for label
+  		
+  		
+  		var search = Search.search('label:' + label.identifier);
+  		
+  		// Create list of top tracks
+  		console.log(search);
+  		var lTopList = List.forCollection(search.tracks, {'layout': 'toplist', 'style': 'rounded', 'numItems': 5});
+  		document.getElementById('lTopList').appendChild(lTopList.node);
+  		lTopList.init();
+  	};
+  var tabBar = TabBar.withTabs([
+    {id: 'overview', name: 'Overview', active: true}
+  ]);
+  tabBar.addToDom(document.getElementById('header'), 'after');
+  // Find items
+  models.application.addEventListener('arguments', function () {
+  	models.application.load('arguments').done(function (a) {
+  		loadLabel(a.arguments);
+  	});
+  })
+  loadLabel(['krikelin']);
 
 });
